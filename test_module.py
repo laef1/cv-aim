45db832b5f559c0736b59478e5c6a01177c4ce27
Leef <manageraccont384@outlook.com>
2025-02-07 13:33:52 -0600

    Update test_module.py

--- test_module.py
+++ test_module.py
@@ -1,7 +1,7 @@
 import aimbot
 
 aimbot.run_aim_alignment(
-    aim_key="RMB",
+    aim_key="RMB", # No other binds besides RMB
     fov_radius=250,
     fov_enabled=True, # Doesn't display, still works | BROKEN
     ai_confidence=0.17,
@@ -11,10 +11,4 @@ aimbot.run_aim_alignment(
     x_offset=-25,
     y_offset=-40
 )
-# cpdef void run_aim_alignment(object aim_key,
-#                              int fov_radius,
-#                              bint fov_enabled,
-#                              double ai_confidence,
-#                              double sensitivity,
-#                              bint gpu,
-#                              bint use_trt):
+
