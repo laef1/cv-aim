22166f401d09c7e256d9ce3de81d064186b6c9f3
Leef <manageraccont384@outlook.com>
2025-02-07 11:07:48 -0600

    Add files via upload

---
+++ test_module.py
@@ -0,0 +1,20 @@
+import aimbot
+
+aimbot.run_aim_alignment(
+    aim_key="RMB",
+    fov_radius=250,
+    fov_enabled=True,
+    ai_confidence=0.17,
+    sensitivity=1.70,
+    gpu=True,
+    use_trt=False,
+    x_offset=-25,
+    y_offset=-40
+)
+# cpdef void run_aim_alignment(object aim_key,
+#                              int fov_radius,
+#                              bint fov_enabled,
+#                              double ai_confidence,
+#                              double sensitivity,
+#                              bint gpu,
+#                              bint use_trt):
